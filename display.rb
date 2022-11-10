5ae894f5bf1c557fa03b52e8c47171ecbff1ec74
Vince <vince.defreitas@gmail.com>
2022-11-10 14:53:00 +0000

    Create player method

--- display.rb
+++ display.rb
@@ -1,19 +1,20 @@
 module Display
 
-  def name
+  def request_name(number)
     "What is the name of Player #{number}?"
   end
 
-  def marker
-    "What letter would you like to be your game marker?"
+  def request_marker
+    'What letter would you like to be your game marker?'
   end
 
-  def enter_number
-    "#{@name}, please enter a number (1-9) that is available to replace an #{@marker}"
+  def enter_number(name, marker)
+    "#{name}, please enter a number (1-9) that is available to replace an #{marker}"
   end
 
   def win
     "GAME OVER! "
+  end
 
 
 
--- game.rb
+++ game.rb
@@ -1,12 +1,35 @@
-require 'player.rb'
-require 'board.rb'
-require_relative 'display.rb'
+require_relative 'display'
+require_relative 'board'
+require_relative 'player'
 
 class Game
   include Display
 
+  attr_accessor :board, :player_one, :player_two, :current_player
 
+  def initialize
+    @board = Board.new
+    @player_one = nil
+    @player_two = nil
+    @current_player = nil
+  end
+
+  def create_player(number)
+    puts request_name(number)
+    name = gets.chomp 
+    puts request_marker
+    marker = gets.chomp
+    Player.new(name, marker)
+  end
 end
+
+game = Game.new
+@player_one = game.create_player(1)
+p @player_one
+
+
+
+
 
 
 
